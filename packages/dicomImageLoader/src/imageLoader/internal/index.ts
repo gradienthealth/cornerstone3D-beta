a7f135c6ec981c8decdfcddb61645e80fd2ac601
Adithyan Dinesh <adithyan.dinesh@trenser.com>
2025-07-29 11:40:58 +0530

    Export the getWadoRsWebServer function

--- packages/dicomImageLoader/src/imageLoader/internal/index.ts
+++ packages/dicomImageLoader/src/imageLoader/internal/index.ts
@@ -1,12 +1,21 @@
 import { default as xhrRequest } from './xhrRequest';
 import { default as streamRequest } from './streamRequest';
 import { setOptions, getOptions } from './options';
+import { getWadoRsWebServer } from './codWebServer';
 
 const internal = {
   xhrRequest,
   streamRequest,
   setOptions,
   getOptions,
+  getWadoRsWebServer,
 };
 
-export { setOptions, getOptions, xhrRequest, internal, streamRequest };
+export {
+  setOptions,
+  getOptions,
+  xhrRequest,
+  internal,
+  streamRequest,
+  getWadoRsWebServer,
+};
