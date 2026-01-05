989444b7c930ef2fe5ef3685144d1c424ea011b3
Adithyan Dinesh <148532903+Adithyan-Dinesh-Trenser@users.noreply.github.com>
2026-01-05 12:14:16 +0530

    Remove the cornerstone decache dependency from CodDicomwebServer cache

--- packages/dicomImageLoader/src/imageLoader/wadors/loadImage.ts
+++ packages/dicomImageLoader/src/imageLoader/wadors/loadImage.ts
@@ -1,7 +1,6 @@
 import {
   Enums,
   imageRetrievalPoolManager,
-  metaData,
   utilities,
 } from '@cornerstonejs/core';
 import { Enums as csCoreEnums, type Types } from '@cornerstonejs/core';
@@ -9,7 +8,6 @@ import { Enums as csCoreEnums, type Types } from '@cornerstonejs/core';
 import createImage from '../createImage';
 import getPixelData from './getPixelData';
 import type { DICOMLoaderIImage, DICOMLoaderImageOptions } from '../../types';
-import { getWadoRsWebServer } from '../internal';
 
 const { ProgressiveIterator } = utilities;
 const { ImageQualityStatus } = Enums;
@@ -215,17 +213,6 @@ function loadImage(
   return {
     promise: uncompressedIterator.getDonePromise(),
     cancelFn: undefined,
-    decache: () => {
-      // Removing the CodDicomwebSever cache here.
-      try {
-        const webServer = getWadoRsWebServer();
-        const instance = metaData.get('instance', imageId);
-        const { DeidSeriesInstanceUID } = instance;
-        webServer.delete(DeidSeriesInstanceUID);
-      } catch (error) {
-        console.warn('Error deleting the CodDicomwebServer cache', error);
-      }
-    },
   };
 }
 
